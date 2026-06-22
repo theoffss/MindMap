82d33477b0e272ec7985ad3cec0429ba276d294a
Jeremy Thomas <jthomas@force-ouvriere.fr>
2026-06-22 13:10:48 +0200

    chore: force PM2 fork mode in ecosystem config and bump version to 0.0.3

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
       name: 'MindMap',
       script: 'server.js',
       instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
